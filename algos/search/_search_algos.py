b0c634cda897e515040b7427bc3ad3e3cfd70a10
Tao Xiang <xiang.tao@outlook.de>
2022-10-29 14:47:06 +0200

    Update _search_algos.py

--- algos/search/_search_algos.py
+++ algos/search/_search_algos.py
@@ -90,63 +90,71 @@ def iterative_deepening_search_for_vis(problem):
             return (iterations, all_node_colors, node)
 
 
+def best_first_graph_search(problem, f):
+    """Search the nodes with the lowest f scores first.
+    You specify the function f(node) that you want to minimize; for example,
+    if f is a heuristic estimate to the goal, then we have greedy best
+    first search; if f is node.depth then we have breadth-first search.
+    There is a subtlety: the line "f = memoize(f, 'f')" means that the f
+    values will be cached on the nodes as they are computed. So after doing
+    a best first search you can examine the f values of the path returned."""
+
+    # we use these two variables at the time of visualisations
+    iterations = 0
+
+    f = memoize(f, 'f')
+    node = Node(problem.initial)
 
 
+    iterations += 1
 
-# Give the cost evaluation method as parameter, then we get UCS
-def uniform_cost_search(problem):
-    def best_first_graph_search(problem, f):
-        """Search the nodes with the lowest f scores first.
-        You specify the function f(node) that you want to minimize; for example,
-        if f is a heuristic estimate to the goal, then we have greedy best
-        first search; if f is node.depth then we have breadth-first search.
-        There is a subtlety: the line "f = memoize(f, 'f')" means that the f
-        values will be cached on the nodes as they are computed. So after doing
-        a best first search you can examine the f values of the path returned."""
 
-        # we use these two variables at the time of visualisations
-        iterations = 0
+    if problem.goal_test(node.state):
+        iterations += 1
+        return (iterations, node)
 
-        f = memoize(f, 'f')
-        node = Node(problem.initial)
+    frontier = PriorityQueue('min', f)
+    frontier.append(node)
+
+
+    iterations += 1
+
+    explored = set()
+    while frontier:
+        node = frontier.pop()
 
 
         iterations += 1
-
 
         if problem.goal_test(node.state):
             iterations += 1
             return (iterations, node)
 
-        frontier = PriorityQueue('min', f)
-        frontier.append(node)
-
-
-        iterations += 1
-
-        explored = set()
-        while frontier:
-            node = frontier.pop()
-
-
-            iterations += 1
-
-            if problem.goal_test(node.state):
+        explored.add(node.state)
+        for child in node.expand(problem):
+            if child.state not in explored and child not in frontier:
+                frontier.append(child)
                 iterations += 1
-                return (iterations, node)
-
-            explored.add(node.state)
-            for child in node.expand(problem):
-                if child.state not in explored and child not in frontier:
+            elif child in frontier:
+                incumbent = frontier[child]
+                if f(child) < f(incumbent):
+                    del frontier[incumbent]
                     frontier.append(child)
                     iterations += 1
-                elif child in frontier:
-                    incumbent = frontier[child]
-                    if f(child) < f(incumbent):
-                        del frontier[incumbent]
-                        frontier.append(child)
-                        iterations += 1
 
-            iterations += 1
-        return None
+        iterations += 1
+    return None
+
+
+# Give the cost evaluation method as parameter, then we get UCS
+def uniform_cost_search(problem):
     return best_first_graph_search(problem, lambda node: node.path_cost)
+
+
+def greedy_best_first_search(problem, h=None):
+    """Greedy Best-first graph search is an informative searching algorithm with f(n) = h(n).
+    You need to specify the h function when you call best_first_search, or
+    else in your Problem subclass."""
+    h = memoize(h or problem.h, 'h')
+    iterations,  node = best_first_graph_search(problem, lambda n: h(n))
+    return(iterations,  node)
